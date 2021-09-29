83e9bd74d974691b82cd0dbf8a9dfbb1ec9f4a5f
nvpost <nvbpost@yandex.ru>
2021-09-29 21:09:55 +0300

    'todo'

--- parse.py
+++ parse.py
@@ -6,6 +6,8 @@ import getProducts
 import getImg
 import os
 
+# TODO В качестве id использовать оригинальные ID для категорий и товаров
+# TODO category = name
 
 from sql import sql
 
@@ -16,8 +18,8 @@ root_node = ET.parse('xml.xml').getroot()
 
 cursor = db.cursor()
 
-# getCategory.get_gategory(root_node, db)
-getProducts.get_products(root_node, db)
+getCategory.get_gategory(root_node, db)
+# getProducts.get_products(root_node, db)
 
 # getImg.get_img(root_node, db)
 
